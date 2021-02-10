b561ae64558b9b4d1d18703cb704b86dd1a62d85
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-09 21:57:55 -0600

    Todo: creando y estructurando la clase Todo-list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import { Todo } from './classes/todo.class.js';
+import { Todo, TodoList } from './classes/index';
 import './styles.css';
 
 
 
+const todoList = new TodoList();
 
 const tarea = new Todo('Aprender JavaScript');
-console.log(tarea);
+const tarea2 = new Todo('Comprar un unicornio');
+
+
+todoList.nuevoTodo( tarea );
+todoList.nuevoTodo( tarea2 );
+
+console.log(todoList);
